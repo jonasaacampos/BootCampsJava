1d773a2a8c69285319573c81461aeae149b3c4be
jaac <38704271+jonasaacampos@users.noreply.github.com>
2022-01-06 11:24:07 -0300

    Finalização do código

--- Aula4_Estruturas-de-repetição/Fatorial.java
+++ Aula4_Estruturas-de-repetição/Fatorial.java
@@ -8,6 +8,15 @@ Ex.: 5!= 120 (5 X 4 X 3 X 2 X 1)
 public class Fatorial {
    public static void main(String[] args) {
       Scanner entrada = new Scanner(System.in);
-       int i 
-   }
+      System.out.println("Insira um numero para saber seu fatorial...");
+      int fatorial = entrada.nextInt();
+      int multiplicacao = 1;
+
+      System.out.print(fatorial + "! = ");
+      for(int i = fatorial; i >= 1; i--){
+         multiplicacao *= i;
+      }
+      System.out.println(multiplicacao);
+      entrada.close();
+}
 }
